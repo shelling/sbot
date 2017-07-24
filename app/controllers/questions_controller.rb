6d558219f5fa65d06b8ad555231cc41b4821bc6f
shelling <navyblueshellingford@gmail.com>
2017-07-24 23:05:00 +0000

    Can respond by query content

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,11 @@ class QuestionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    payload = JSON.parse(params[:payload])
     render json: {
-      text: "Payload Response",
       attachments: [
         {
-          text: "Attachment",
+          text: "Answer to #{payload["actions"].first["name"]}",
           fallback: "No Attachment",
           callback_id: "1",
           color: "#3AA3E3",
